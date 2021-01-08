a5efe69cdac3cae8d3e9ab94acaefd503661f5a0
Ivo <ivo_prr@hotmail.com>
2021-01-08 19:40:43 -0300

    Add files via upload

---
+++ bnotas/Bnotas.java
@@ -0,0 +1,131 @@
+package bnotas;
+
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+
+/**
+ *
+ * @author Ivo
+ */
+public class Bnotas extends JFrame {
+
+    private final JEditorPane texto = new JEditorPane();
+
+    public JEditorPane getTexto() {
+        return texto;
+    }
+
+    private final JScrollPane scroll = new JScrollPane(texto);
+
+    private int teclaPressionada = -1;
+
+    //private static Bnotas janelaAcess;
+    // Hue a;
+    public static Funcoes f = new Funcoes();
+
+    Bnotas() {
+        super("BNotas");
+        this.montaJanela();
+        this.Caracteristicas();
+    }
+
+    private void montaJanela() {
+        this.add(scroll);
+        this.texto.addKeyListener(new java.awt.event.KeyAdapter() {
+            @Override
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                if (teclaPressionada == -1) {
+                    teclaPressionada = evt.getKeyCode();
+                }
+                if (teclaPressionada == KeyEvent.VK_ALT) {
+                    switch (evt.getKeyCode()) {
+                        case KeyEvent.VK_C:
+                            f.trocaCor();
+                            teclaPressionada = -1;
+                            break;
+                        case KeyEvent.VK_A:
+                            f.abrir();
+                            teclaPressionada = -1;
+                            break;
+                        case KeyEvent.VK_S:
+                            try {
+                                f.salvar();
+                            } catch (IOException ex) {
+                                Logger.getLogger(Bnotas.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                            teclaPressionada = -1;
+                            break;
+                        case KeyEvent.VK_T:;
+                            f.subst();
+                            teclaPressionada = -1;
+                            break;
+                        case KeyEvent.VK_B:
+                            f.trocaBackGround();
+                            teclaPressionada = -1;
+                            break;
+                    }
+                } else {
+                    teclaPressionada = -1;
+                }
+
+            }
+        });
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Bnotas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+    }
+
+    private void Caracteristicas() {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //janelaAcess = janela;
+        f.setBnotas(this);
+        /* A medida de tamanho é em pixels por polegada, igual a da resolução da sua tela */
+        //Formas de definir o tamanho da tela
+        this.setSize(640, 480);
+        //janela.pack(); << define o tamanho automáticamente //n recomendavel no caso
+        this.setVisible(true);
+    }
+
+    public class Hue extends Thread {
+
+        /* boolean rodando = true;
+        
+        public void mata() {
+        rodando = false;
+        }
+        
+        @Override
+        public void run() {
+        
+        while (rodando) {
+        
+        System.out.println("Oi!");
+        
+        if (!janelaAcess.isVisible()) {
+        mata();
+        }
+        try {
+        sleep(100);
+        } catch (InterruptedException ex) {
+        ex.getMessage();
+        }
+        }
+        }*/
+    }
+
+    public static void main(String[] args) {
+        Bnotas janela = new Bnotas();
+    }
+}
---
+++ bnotas/Funcoes.java
@@ -0,0 +1,100 @@
+package bnotas;
+
+import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JColorChooser;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Ivo
+ */
+public class Funcoes {
+
+    Bnotas bn;
+
+    public void setBnotas(Bnotas b) {
+        bn = b;
+    }
+
+    public boolean trocaCor() {
+        Color color = null;
+        color = JColorChooser.showDialog(bn, "Selecione uma cor", color);
+        bn.getTexto().setForeground(color);
+        return true;
+    }
+
+    public boolean trocaBackGround() {
+        Color color = null;
+        color = JColorChooser.showDialog(bn, "Selecione uma cor", color);
+        bn.getTexto().setBackground(color);
+        return true;
+    }
+
+    public void salvar() throws IOException {
+        JFileChooser file = new JFileChooser();
+        file.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        int retorno = file.showSaveDialog(bn);
+        if (retorno != JFileChooser.APPROVE_OPTION) {
+            return;
+        }
+        FileWriter arq = new FileWriter(file.getSelectedFile());
+        PrintWriter gravarArq = new PrintWriter(arq);
+        gravarArq.printf(bn.getTexto().getText());
+        try {
+            arq.close();
+        } catch (IOException ex) {
+            Logger.getLogger(bnotas.Bnotas.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void abrir() {
+        JFileChooser file = new JFileChooser();
+        file.setFileSelectionMode(JFileChooser.OPEN_DIALOG);
+        int retorno = file.showOpenDialog(bn);
+        if (retorno != JFileChooser.APPROVE_OPTION) {
+            return;
+        }
+        try {
+            FileReader arq = new FileReader(file.getSelectedFile());
+            BufferedReader lerArq = new BufferedReader(arq);
+            String c = null;
+            String linha = lerArq.readLine();
+            while (linha != null) {
+                if (c == null) {
+                    c = linha + "\n";
+                } else {
+                    c += linha + "\n";
+                }
+                linha = lerArq.readLine();
+            }
+            System.out.println(c);
+            bn.getTexto().setText(c);
+        } catch (IOException e) {
+            System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+        }
+    }
+
+    // public void formatar(){}
+    public void subst() {
+        String a;
+        String subs = JOptionPane.showInputDialog(bn, "digite o que você quer substituir:");
+        String subst = JOptionPane.showInputDialog(bn, "digite o que você quer no lugar de: " + subs);
+        if (!subs.equals("")) {
+            a = bn.getTexto().getText();
+            a = a.replace(subs, subst);
+            bn.getTexto().setText(a);
+        } else {
+            a = bn.getTexto().getText();
+            a += subst;
+            bn.getTexto().setText(a);
+        }
+    }
+}
